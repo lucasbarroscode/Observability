7f846b0252992a8e25ec49a969992635ec976993
lucasbarroscode <lucasbabezerra@gmail.com>
2024-05-14 09:40:10 -0300

    featÃ: criando vendaRequestDTO

---
+++ src/main/java/com/lucascode/gvendas/gestaovendas/dto/venda/VendaRequestDTO.java
@@ -0,0 +1,40 @@
+package com.lucascode.gvendas.gestaovendas.dto.venda;
+
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+import java.util.List;
+
+@ApiModel("Venda requisição DTO")
+public class VendaRequestDTO {
+
+    @ApiModelProperty(value = "Data")
+    @NotNull(message = "Data")
+    private LocalDate data;
+
+    @ApiModelProperty(value = "Itens da venda")
+    @NotNull(message = "Itens venda")
+    @Valid
+    private List<ItemVendaRequestDTO> itensVendaDto;
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    public List<ItemVendaRequestDTO> getItensVendaDto() {
+        return itensVendaDto;
+    }
+
+    public void setItensVendaDto(List<ItemVendaRequestDTO> itensVendaDto) {
+        this.itensVendaDto = itensVendaDto;
+    }
+
+}
