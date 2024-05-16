4c799391ccbad1ff9618d49d6986498ae51d81bc
lucasbarroscode <lucasbabezerra@gmail.com>
2024-05-15 22:47:24 -0300

    feat: registro de vendas

--- src/main/java/com/lucascode/gvendas/gestaovendas/controller/VendaController.java
+++ src/main/java/com/lucascode/gvendas/gestaovendas/controller/VendaController.java
@@ -1,15 +1,14 @@
 package com.lucascode.gvendas.gestaovendas.controller;
 
 import com.lucascode.gvendas.gestaovendas.dto.venda.ClienteVendaResponseDTO;
+import com.lucascode.gvendas.gestaovendas.dto.venda.VendaRequestDTO;
 import com.lucascode.gvendas.gestaovendas.services.VendaService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Api(tags = "Venda")
 @RestController
@@ -29,6 +28,12 @@ public class VendaController {
     @GetMapping("/{codigoVenda}")
     public ResponseEntity<ClienteVendaResponseDTO> listarVendaPorCodigo(@PathVariable Long codigoVenda){
         return ResponseEntity.ok(vendaService.listarVendaPorCodigo(codigoVenda));
+    }
+
+    @ApiOperation(value = "Registrar Venda", nickname = "salvar")
+    @PostMapping("/cliente/{codigoCliente}")
+    public ResponseEntity<ClienteVendaResponseDTO> salvar(@PathVariable Long codigoCliente, @RequestBody VendaRequestDTO vendaDto){
+        return ResponseEntity.status(HttpStatus.CREATED).body(vendaService.salvar(codigoCliente, vendaDto));
     }
 
 
